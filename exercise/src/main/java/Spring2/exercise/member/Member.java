377e349ba8e367a00e88878456725aa91a0c823c
sugamypapa <sk000801@naver.com>
2022-07-25 00:17:36 +0900

    Error Edit
    so tired..

--- exercise/src/main/java/Spring2/exercise/member/Member.java
+++ exercise/src/main/java/Spring2/exercise/member/Member.java
@@ -3,25 +3,24 @@ package Spring2.exercise.member;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@Table(name="members")
 public class Member {
 
     private String name;
+
     @Id
     @GeneratedValue
+    @Column(name="member_id")
     private Long id;
+
     private String city;
+
     private int distance;
 
-    public Member(String name, String city, int distance) {
-        this.name = name;
-        this.city = city;
-        this.distance = distance;
-    }
+
  }
