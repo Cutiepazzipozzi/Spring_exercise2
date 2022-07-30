e9aefbcf7c005343a76b925e3f6ac82c1ac790c0
sugamypapa <sk000801@naver.com>
2022-07-30 16:39:47 +0900

    fix: Member Controller

--- exercise/src/main/java/Spring2/exercise/controller/OrderController.java
+++ exercise/src/main/java/Spring2/exercise/controller/OrderController.java
@@ -1,39 +1,42 @@
-//package Spring2.exercise.controller;
-//
-//import Spring2.exercise.order.Order;
-//import Spring2.exercise.order.OrderRepository;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import java.util.List;
-//
-//@Controller
-//public class OrderController {
-//
-//    OrderRepository orderRepository;
-//
-//    @GetMapping("/orders/join")
-//    public String join() {
-//        return "orders/joinOrder";
-//    }
-//
-//    @PostMapping("/orders/join")
-//    public String enroll(OrderForm form) {
-//        Order order = new Order(form.getPName(), form.getPrice(), form.getPNumber());
-//        order.setPName(form.getPName());
-//        order.setPrice(form.getPrice());
-//        order.setPNumber(form.getPNumber());
-//
-//        orderRepository.join(order);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/orders/list")
-//    public String list(Model model) {
-//        List<Order> orders = orderRepository.findAll();
-//        model.addAttribute("orders", orders);
-//        return "orders/orderList";
-//    }
-//}
+package Spring2.exercise.controller;
+
+import Spring2.exercise.order.Order;
+import Spring2.exercise.order.OrderRepository;
+import Spring2.exercise.order.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/orders/join")
+    public String join() {
+        return "orders/joinOrder";
+    }
+
+    @PostMapping("/orders/join")
+    public String enroll(OrderForm form) {
+        Order order = new Order();
+        order.setMember(form.getMember());
+        order.setPName(form.getPName());
+        order.setPNumber(form.getPNumber());
+
+        orderService.order(form.getMember().getId(), );
+        return "redirect:/";
+    }
+
+    @GetMapping("/orders/list")
+    public String list(Model model) {
+        List<Order> orders = orderRepository.findAll();
+        model.addAttribute("orders", orders);
+        return "orders/orderList";
+    }
+}
