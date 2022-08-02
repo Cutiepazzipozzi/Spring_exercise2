b2d2c644bb3323f1b6f029d78c477d8833781502
sugamypapa <sk000801@naver.com>
2022-08-02 11:20:21 +0900

    fit: order edit + search 연결
    대실패...

--- exercise/src/main/java/Spring2/exercise/controller/OrderController.java
+++ exercise/src/main/java/Spring2/exercise/controller/OrderController.java
@@ -3,6 +3,7 @@ package Spring2.exercise.controller;
 import Spring2.exercise.member.Member;
 import Spring2.exercise.member.MemberService;
 import Spring2.exercise.order.Order;
+import Spring2.exercise.order.OrderSearch;
 import Spring2.exercise.order.OrderService;
 import Spring2.exercise.product.Product;
 import Spring2.exercise.product.ProductService;
@@ -37,35 +38,25 @@ public class OrderController {
     public String enroll(@RequestParam("memberId") Long memberId,
                 @RequestParam("productId") Long productId,
                          @RequestParam("count") int count) {
-//        Product product = new Product();
-//        Member member = new Member();
-//
-//        member.setId(form.getMember().getId());
-//        Long memberId = member.getId();
-//
-//        product.setId(productService.findByName(form.getPName()).getId());
-//        Long productId = product.getId();
-//
-//        int count = form.getPNumber();
-
         orderService.order(memberId, productId, count);
         return "redirect:/";
   }
 
-    @GetMapping("/orders/list")
-    public String list(Model model) {
-        List<Order> orders = orderService.findAll();
-        model.addAttribute("orders", orders);
-        return "orders/orderList";
-    }
 
     //보엣 주문 조회가 안될 듯함...
     //이거 말고
-    @PostMapping("/orders/list/{id}")
-    public String delete(@PathVariable("id") Long orderId) {
+    @PostMapping("/orders/list/{orderId}")
+    public String delete(@PathVariable("orderId") Long orderId) {
         orderService.cancelOrder(orderId);
         return "redirect:/";
     }
 
+    @GetMapping("/orders/list")
+    public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderSearch.findAll(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "orders/orderList";
+    }
 
 }
